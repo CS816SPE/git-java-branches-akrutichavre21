63dc6ac14c41743888ff23f136da134bbf1389c9
akruti <akrutichavre21@gmail.com>
2019-01-17 10:33:06 +0530

    add functionality

---
+++ Caladd.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Caladd {
+	public static void main(String args[]) {
+		float a,b,res;
+		char choice,ch;
+		Scanner scan = new Scanner(System.in);
+
+		do {
+			System.out.print("\n1. MUL two numbers \n");
+			System.out.print("2. Exit \n");
+			System.out.print("Enter your choice : \n");	
+			choice = scan.next().charAt(0);
+
+			switch(choice){
+			case '1': System.out.println("Enter two numbers:");
+			a = scan.nextFloat();
+			b = scan.nextFloat();
+			res = a*b;
+			System.out.println("Result = " + res);
+			break;
+			
+			case '2':System.exit(0);
+			break;
+			
+			default : System.out.println("Wrong choice");
+			break;
+
+			}
+		
+		System.out.println("_______________________________________________________");
+
+		}while(choice!=2);
+
+
+	}
+}
